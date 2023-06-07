f9d3595ab21b53746f7e9db40c98bc58d30fe339
salomaotech <salomao@taimber.com>
2023-06-07 19:49:06 -0300

    5 - Estrutura de dado - Lista ligada

--- src/main/java/br/com/salomaotech/App.java
+++ src/main/java/br/com/salomaotech/App.java
@@ -2,6 +2,8 @@ package br.com.salomaotech;
 
 import br.com.salomaotech.arrays.ArrayExemplo;
 import br.com.salomaotech.fila.Impressora;
+import br.com.salomaotech.lista.ListaLigada;
+import br.com.salomaotech.lista.No;
 import br.com.salomaotech.pilha.HistoricoDeNavegacao;
 
 public class App {
@@ -41,6 +43,31 @@ public class App {
         impressora.addDocumento("Documento C");
         impressora.addDocumento("Com este livro, você irá partir do nível zero e alcançará um conhecimento intermediário em Java! Com explicações claras e exemplos práticos, você será guiado de forma gradual pelos conceitos básicos dessa linguagem de programação. ");
         impressora.imprimir();
+
+    }
+
+    private static void listaLigada() {
+
+        ListaLigada<String> lista = new ListaLigada();
+        lista.add("A");
+        lista.add("B");
+        lista.add("C");
+        lista.add("D");
+        lista.add("E");
+
+        lista.remove(2);
+
+        No[] nos = lista.getList();
+
+        for (No no : nos) {
+
+            if (no.getProximo() != null) {
+
+                System.out.println("Atual (" + no.getConteudo() + ") próximo (" + no.getProximo().getConteudo() + ")");
+
+            }
+
+        }
 
     }
 
