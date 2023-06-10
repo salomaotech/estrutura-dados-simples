8d54c44c59ba86ed3a44803040fd7d0aa4222e0f
salomaotech <salomao@taimber.com>
2023-06-10 16:11:21 -0300

    8 - Estrutura de dado - Arvore

--- src/main/java/br/com/salomaotech/App.java
+++ src/main/java/br/com/salomaotech/App.java
@@ -1,15 +1,14 @@
 package br.com.salomaotech;
 
 import br.com.salomaotech.arrays.ArrayExemplo;
+import br.com.salomaotech.arvore.ArvoreBinaria;
 import br.com.salomaotech.conjunto.Conjunto;
 import br.com.salomaotech.fila.Impressora;
+import br.com.salomaotech.grafo.Grafo;
 import br.com.salomaotech.lista.ListaLigada;
 import br.com.salomaotech.lista.No;
 import br.com.salomaotech.mapa.Mapa;
 import br.com.salomaotech.pilha.HistoricoDeNavegacao;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class App {
 
@@ -126,18 +125,79 @@ public class App {
 
     }
 
+    private static void arvoreBinaria() {
+
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+        arvoreBinaria.inserir(33);
+        arvoreBinaria.inserir(20);
+        arvoreBinaria.inserir(2);
+        arvoreBinaria.inserir(45);
+        arvoreBinaria.inserir(8);
+        arvoreBinaria.inserir(28);
+        arvoreBinaria.inserir(39);
+        arvoreBinaria.inserir(22);
+        arvoreBinaria.inserir(5);
+        arvoreBinaria.inserir(48);
+        arvoreBinaria.inserir(35);
+        arvoreBinaria.inserir(18);
+        arvoreBinaria.inserir(44);
+        arvoreBinaria.inserir(38);
+        arvoreBinaria.inserir(42);
+        arvoreBinaria.inserir(26);
+        arvoreBinaria.inserir(31);
+        arvoreBinaria.inserir(25);
+        arvoreBinaria.inserir(14);
+        arvoreBinaria.inserir(43);
+        arvoreBinaria.inserir(40);
+        arvoreBinaria.inserir(13);
+        arvoreBinaria.inserir(29);
+        arvoreBinaria.inserir(12);
+        arvoreBinaria.inserir(11);
+        arvoreBinaria.inserir(49);
+        arvoreBinaria.inserir(23);
+        arvoreBinaria.inserir(41);
+        arvoreBinaria.inserir(1);
+        arvoreBinaria.inserir(19);
+        arvoreBinaria.inserir(10);
+        arvoreBinaria.inserir(15);
+        arvoreBinaria.inserir(6);
+        arvoreBinaria.inserir(24);
+        arvoreBinaria.inserir(46);
+        arvoreBinaria.inserir(30);
+        arvoreBinaria.inserir(47);
+        arvoreBinaria.inserir(4);
+        arvoreBinaria.inserir(9);
+        arvoreBinaria.inserir(16);
+        arvoreBinaria.inserir(36);
+        arvoreBinaria.inserir(7);
+        arvoreBinaria.inserir(21);
+        arvoreBinaria.inserir(0);
+        arvoreBinaria.inserir(34);
+        arvoreBinaria.inserir(27);
+        arvoreBinaria.inserir(3);
+        arvoreBinaria.inserir(32);
+        arvoreBinaria.inserir(50);
+        arvoreBinaria.inserir(17);
+        arvoreBinaria.inserir(37);
+        arvoreBinaria.percorrerOrdem();
+
+    }
+
+    private static void grafo() {
+
+        Grafo grafo = new Grafo(5);
+        grafo.adicionarAresta(0, 4);
+        grafo.adicionarAresta(1, 3);
+        grafo.adicionarAresta(2, 2);
+        grafo.adicionarAresta(3, 1);
+        grafo.adicionarAresta(4, 0);
+        grafo.adicionarAresta(5, 3);
+        grafo.adicionarAresta(5, 2);
+        grafo.imprimirGrafo();
+
+    }
+
     public static void main(String[] args) {
-
-        Map<String, String> mapa = new LinkedHashMap();
-        mapa.put("www.salomaotech.com", "2a02:4780:13:918:0:1d22:12ad:3");
-        mapa.put("www.google.com", "2800:3f0:4001:822::2004");
-        mapa.put("www.taimber.com", "2a02:4780:13:918:0:1d22:12ad:2");
-
-        for (Map.Entry<String, String> dadosDoMapa : mapa.entrySet()) {
-
-            System.out.println(dadosDoMapa.getKey() + " >>> " + dadosDoMapa.getValue());
-
-        }
 
     }
 
